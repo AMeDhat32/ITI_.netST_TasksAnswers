33b6aadbeabe36442dda9a70e1b3d4d8335a1826
AMeDhat <bodey146@gmail.com>
2024-08-24 11:53:36 +0300

    Add Day5&6 Task

---
+++ ITI_task_D5_D6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iti_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the first number");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("choose the operation \n1)+\n2)-\n3)*\n4)/");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            
+            switch (op)
+            {
+                case '+':                
+                    Console.WriteLine("result : " + (a + b));
+                    break;
+                case '-':
+                    Console.WriteLine("result : " + (a - b));
+                    break;
+                case '*':
+                    
+                    Console.WriteLine("result : " + (a * b));
+                    break;
+                case '/':
+                    if (b == 0)
+                    {
+                        Console.WriteLine("You can't divide by zero");
+                    }else
+                    {
+                        Console.WriteLine("result : " + (a / b));
+                    }
+                    break;
+
+            }
+        }
+    }
+}
